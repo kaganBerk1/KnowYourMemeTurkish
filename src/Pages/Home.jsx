c4ba2491be029ab790bffa7a63b775500c3f8093
Elbis1 <64051166+Elbis1@users.noreply.github.com>
2022-07-04 00:55:58 +0300

    information pages added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Header from '../components/header/Header'
+import Information from '../components/Informations/Information'
 export default function Home() {
   return (
     <div>
         <Header></Header>
+        <Information></Information>
         Home
     </div>
   )
