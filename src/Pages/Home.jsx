80f7054585632dbc7a84777d94d65a5a4adc7a39
Elbis1 <64051166+Elbis1@users.noreply.github.com>
2022-07-04 01:39:06 +0300

    last added memes componenet added to home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from '../components/header/Header'
 import Information from '../components/Informations/Information'
+import ThreeMemes from '../components/memes/ThreeMemes'
 export default function Home() {
   return (
     <div>
         <Header></Header>
         <Information></Information>
-        Home
+        <ThreeMemes></ThreeMemes>
     </div>
   )
 }
