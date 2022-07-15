a17afc1451794fccd0d91a1a455918192d1fa839
Elbis1 <64051166+Elbis1@users.noreply.github.com>
2022-07-16 02:46:44 +0300

    css style and gif added

--- src/Pages/Memes.jsx
+++ src/Pages/Memes.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import Header from '../components/header/Header'
 import { Link, useLocation } from "react-router-dom";
+import dots from "../images/dots.gif"
 import "./Memes.scss"
 import { useEffect } from 'react';
 import axios from '../Api/axios'
 export default function Memes() {
   const [memes,setMemes]= React.useState([]);
+  const [loading,setLoading]= React.useState(false);
+
   const location = useLocation();
  useEffect(()=>{
     let path=window.location.search
+    setLoading(true)
     if(path.includes("searchText")){
       getMemesBySearchText();
     }else{
       getAllMemes();
     }
+    setLoading(false)
  },[location])
  function getAllMemes(){
     axios.get("/api/memes").then(res => {
@@ -42,21 +47,40 @@ export default function Memes() {
   return (
     <div> 
       <Header></Header>
+    {
+      !loading?
       <div className='Cover'>
         {
-         memes.map((el)=>{
-              return(
-                  <Link className='wholeMemes' to={`/memes/${el._id}`} key={el.id}>
-                    <img src={el.memeImage}></img>
-                    <div className='memeDescription'>{el.title}</div>
-                    <span className='writer'>
-                      {el.admin}
-                      </span>
-                  </Link>
-              )
-          })
+          !memes.length>0
+          ?
+          <div className='Cover' >
+              <span className='noFound'>
+                Sorry, We do not find your search!
+              </span>
+          </div>
+          :
+          <>
+          {
+          memes.map((el)=>{
+                return(
+                    <Link className='wholeMemes' to={`/memes/${el._id}`} key={el.id}>
+                      <img src={el.memeImage}></img>
+                      <div className='memeDescription'>{el.title}</div>
+                      <span className='writer'>
+                        {el.admin}
+                        </span>
+                    </Link>
+                )
+            })
+          }
+      </>
         }
-      </div>
+    </div>
+    :
+    <div style={{display:"flex",justifyContent:"center"}}>
+      <img style={{width:"80px",height:"80px"}} src={dots} alt="loading..." />
+    </div>
+    }
     </div>
   )
 }
--- src/components/memes/ThreeMemes.jsx
+++ src/components/memes/ThreeMemes.jsx
@@ -3,15 +3,16 @@ import "./ThreeMemes.scss"
 import Carousel from 'react-elastic-carousel';
 import axios from '../../Api/axios';
 import { Link } from 'react-router-dom';
+import dots from "../../images/dots.gif"
 export default function ThreeMemes() {
  const [memes,setMemes] = React.useState([]) 
  const [width, setWidth] = React.useState(window.innerWidth);
-
+ const [loading,setLoading]= React.useState(false);
  React.useEffect(() => {
-
+   setLoading(true)
    window.addEventListener("resize", () => setWidth(window.innerWidth));
    getThreeMeme();
-
+   setLoading(false)
  }, []);
 
  function getThreeMeme(){
@@ -27,19 +28,27 @@ export default function ThreeMemes() {
     <div className='Container'>
         <span className='titleThreeMeme'>Son Eklenenler</span>
         <div className='memesContainer'>
-        <Carousel className='carousel' tiltEasing='ease' enableAutoPlay={true} autoPlaySpeed={5000} showArrows={false} itemsToShow={(width>1300)?3:width>720?2:1}>
         {
-            memes.map((el)=>{
-                return(
-                    <Link className='memeContainer' to={`/memes/${el._id}`} key={el.id}>
-                        <span className='writer'>Writer: {el.admin}</span>
-                        <img  className="memeImage" src={el.memeImage} alt="" />
-                        <div className='memeDescription'>{el.title}</div>
-                    </Link>
-                )
-            })
+            loading ?
+            <div style={{display:"flex",justifyContent:"center"}}>
+                    <img style={{width:"80px",height:"80px"}} src={dots} alt="loading..." />
+            </div>
+            
+            :
+            <Carousel className='carousel' tiltEasing='ease' enableAutoPlay={true} autoPlaySpeed={5000} showArrows={false} itemsToShow={(width>1300)?3:width>720?2:1}>
+                {
+                    memes.map((el)=>{
+                        return(
+                            <Link className='memeContainer' to={`/memes/${el._id}`} key={el.id}>
+                                <span className='writer'>Writer: {el.admin}</span>
+                                <img  className="memeImage" src={el.memeImage} alt="" />
+                                <div className='memeDescription'>{el.title}</div>
+                            </Link>
+                        )
+                    })
+                }
+            </Carousel>
         }
-       </Carousel>
         </div>
     </div>
   )
